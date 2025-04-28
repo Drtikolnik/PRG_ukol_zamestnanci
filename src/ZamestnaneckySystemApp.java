a1cefb0eff3c757dc5a63406db79b6dbf343bf45
Drtikolnik <drtluk00@gasos-ro.cz>
2025-04-28 12:11:40 +0200

    kdyby byl skibidy

--- src/ZamestnaneckySystemApp.java
+++ src/ZamestnaneckySystemApp.java
@@ -2,11 +2,19 @@ import firmapozice.Manager;
 import firmapozice.Tester;
 import firmapozice.Vyvojar;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ZamestnaneckySystemApp {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+
+
+    ArrayList<Manager> manageri = new ArrayList<>();
+    ArrayList<Tester> testeri = new ArrayList<>();
+    ArrayList<Vyvojar> vyvojari = new ArrayList<>();
+
+
 
     //MANAGER----------------------------------
     System.out.println("Jméno Managera:");
@@ -26,10 +34,11 @@ public class ZamestnaneckySystemApp {
     int pocetRizenychZamestnancuM = sc.nextInt();
     sc.nextLine();
 
-    Manager manager = new Manager(jmenoM, prijmeniM, poziceM, platM, pocetRizenychZamestnancuM);
+    manageri.add(new Manager(jmenoM, prijmeniM, poziceM, platM, pocetRizenychZamestnancuM));
 
 
     //TESTER----------------------------------
+        System.out.println("");
         System.out.println("Jméno Testera:");
         String jmenoT = sc.nextLine();
 
@@ -43,11 +52,44 @@ public class ZamestnaneckySystemApp {
         int platT = sc.nextInt();
         sc.nextLine();
 
-        System.out.println("Počet nalezených chyb Testerem:");
+        System.out.println("Počet nalezených chyb Testerem (ideálně zadej 0):");
         int pocetNalezenychChybT = sc.nextInt();
         sc.nextLine();
 
-        Tester tester = new Tester(jmenoM, prijmeniM, poziceM, platM, pocetNalezenychChybT);
+        testeri.add(new Tester(jmenoT, prijmeniT, poziceT, platT, pocetNalezenychChybT));
+
+        /*
+        for(int ixxx=0; ixxx==20;ixxx++){
+            tester.naselJsemNovouChybu();
+        }
+         */
+
+        //VYVOJAR----------------------------------
+        System.out.println("");
+        System.out.println("Jméno Vývojáře:");
+        String jmenoV = sc.nextLine();
+
+        System.out.println("Příjmení Vývojáře:");
+        String prijmeniV = sc.nextLine();
+
+        System.out.println("Pozice Vývojáře:");
+        String poziceV = sc.nextLine();
+
+        System.out.println("Plat Vývojáře:");
+        int platV = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Známé programovací jazyky:");
+        System.out.println("Kolik jazyků zná?:");
+        int pocetJazyky = sc.nextInt();
+        sc.nextLine();
+        String jazyky[] = new String[pocetJazyky];
+        System.out.println("Jaké jazyky zná?:");
+        for(int ixxxx=0;ixxxx<pocetJazyky;ixxxx++){
+            jazyky[ixxxx] = sc.nextLine();
+        }
+
+        vyvojari.add(new Vyvojar(jmenoV, prijmeniV, poziceV, platV, jazyky));
 
 
 
